f5c0d5d7ac886577b8314df37d1afa078246ca40
niggiover9000 <nicolasbartels@web.de>
2020-04-10 12:23:19 +0200

    Created a class for sharing a var.

--- Arbeitzeit.py
+++ Arbeitzeit.py
@@ -3,56 +3,84 @@ from os import path
 from shutil import copyfile
 
 
-def startup():
-    accuracy = -1
-    while accuracy <= 1:
-        accuracy_input = input("Genauigkeit in Minuten (mindestens 1, maximal 60): ")
+class Arbeitszeit:
+    def __init__(self):
+        self.accuracy = -1
+
         try:
-            accuracy = int(accuracy_input)
-            if accuracy <= 1 <= 60:
-                raise ValueError("Zahl nicht gültig")
-        except (ValueError, TypeError) as Error:
+            file = open("localization/language.setting", "r")
+            if file.read() == "DE":
+                import localization.de as language
+                self.language = "DE"
+            elif file.read() == "EN":
+                import localization.en as language
+                self.language = "EN"
+            else:
+                raise OSError("Config file not readable.")
+        except OSError as Error:
             print(Error)
-            continue
-    print(f"Die Zeit wird auf {accuracy} Minuten genau erfasst.")
-    open("arbeitszeit.temp", "w").close()
-    return accuracy
+            input("Choose a language / Wähle eine Sprache: (Type 'en' for English and 'de' for German): ")
+
+    def set_accuracy(self):
+        while self.accuracy <= 1:
+            accuracy_input = input("Genauigkeit in Minuten (mindestens 1, maximal 60): ")
+            try:
+                self.accuracy = int(accuracy_input)
+                if self.accuracy <= 1 <= 60:
+                    raise ValueError("Zahl nicht gültig")
+            except (ValueError, TypeError) as Error:
+                print(Error)
+                continue
+        print(f"Die Zeit wird auf {self.accuracy} Minuten genau erfasst.")
+        open("arbeitszeit.temp", "w").close()
+
+    def begin_working(self):
+        activity_time = localtime(time())
+        accuracy_time = activity_time[4] % self.accuracy
+        activity_minutes = activity_time[4] - accuracy_time
+        file = open("arbeitszeit.temp", "a")
+        file.write(f"[{activity_time[3]}:{activity_minutes}] --ARBEITSBEGINN--\n")
+        file.close()
+        print(f"Die Arbeitszeit wird jetzt erfasst. Du hast um {activity_time[3]}:{activity_minutes} angefangen.")
 
 
-def begin_working(accuracy):
-    activity_time = localtime(time())
-    accuracy_time = activity_time[4] % accuracy
-    activity_minutes = activity_time[4] - accuracy_time
-    print(f"Die Arbeitszeit wird jetzt erfasst. Du hast um {activity_time[3]}:{activity_minutes} angefangen.")
+    def user_input(self):
+        print("Info: '/end' eingeben, um die Erfassung zu beenden und eine Tätigkeitsliste zu exportieren.")
+        print("'/pause' eingeben, um eine Pause zu erfassen.")
+        working = True
+        while working is True:
+            user_input = input("Tätigkeit erfassen: ")
+            if user_input == "/end":
+                filename = strftime("[%d.%m.%Y, %H.%M.%S] Arbeitszeit.txt", localtime())
+                pathname = path.dirname(path.realpath(__file__))
+                activity_time = localtime(time())
+                accuracy_time = activity_time[4] % self.accuracy
+                activity_minutes = activity_time[4] + (self.accuracy - accuracy_time)
+                file = open("arbeitszeit.temp", "a")
+                file.write(f"[{activity_time[3]}:{activity_minutes}] Arbeitszeiterfassung beendet.\n")
+                file.close()
+                copyfile("arbeitszeit.temp", f"{filename}")
+                print(f"Gute Arbeit! Die Datei wurde als '{filename}' unter '{pathname}' gespeichert. Schönen Feierabend!")
+                working = False
+            elif user_input == "/pause":
+                activity_time = localtime(time())
+                accuracy_time = activity_time[4] % self.accuracy
+                activity_minutes = activity_time[4] + (self.accuracy - accuracy_time)
+                file = open("arbeitszeit.temp", "a")
+                file.write(f"[{activity_time[3]}:{activity_minutes}] --PAUSE--\n")
+                file.close()
+                print("Entspannte Pause!")
+            else:
+                activity_time = localtime(time())
+                accuracy_time = activity_time[4] % self.accuracy
+                activity_minutes = activity_time[4] - accuracy_time
+                file = open("arbeitszeit.temp", "a")
+                file.write(f"[{activity_time[3]}:{activity_minutes}] {user_input}\n")
+                file.close()
+                print(f"Um {activity_time[3]}:{activity_minutes} wurde die Tätigkeit '{user_input}' erfasst.")
 
 
-def user_input(accuracy):
-    print("Info: '/end' eingeben, um die Erfassung zu beenden und eine Tätigkeitsliste zu exportieren!")
-    working = True
-    while working is True:
-        user_input = input("Tätigkeit erfassen: ")
-        if user_input == "/end":
-            filename = strftime("[%d.%m.%Y, %H.%M.%S] Arbeitszeit.txt", localtime())
-            pathname = path.dirname(path.realpath(__file__))
-            activity_time = localtime(time())
-            accuracy_time = activity_time[4] % accuracy
-            activity_minutes = activity_time[4] + (accuracy - accuracy_time)
-            file = open("arbeitszeit.temp", "a")
-            file.write(f"[{activity_time[3]}:{activity_minutes}] Arbeitszeiterfassung beendet.\n")
-            file.close()
-            copyfile("arbeitszeit.temp", f"{filename}")
-            print(f"Gute Arbeit! Die Datei wurde als '{filename}' unter '{pathname}' gespeichert. Schönen Feierabend!")
-            working = False
-        else:
-            activity_time = localtime(time())
-            accuracy_time = activity_time[4] % accuracy
-            activity_minutes = activity_time[4] - accuracy_time
-            file = open("arbeitszeit.temp", "a")
-            file.write(f"[{activity_time[3]}:{activity_minutes}] {user_input}\n")
-            file.close()
-            print(f"Um {activity_time[3]}:{activity_minutes} wurde die Tätigkeit '{user_input}' erfasst.")
-
-
-accuracy = startup()
-begin_working(accuracy)
-user_input(accuracy)
+arbeiten = Arbeitszeit()
+arbeiten.set_accuracy()
+arbeiten.begin_working()
+arbeiten.user_input()
